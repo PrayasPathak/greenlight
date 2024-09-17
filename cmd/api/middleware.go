2011747017af1ca3ad955887352d9a979d0d152b
PrayasPathak <prayasroman1@gmail.com>
2024-09-17 17:17:01 +0545

    Add preflight cors settings for application

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -169,13 +169,23 @@ func (app *application) requirePermission(code string, next http.HandlerFunc) ht
 func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Vary", "Origin")
+		w.Header().Add("Vary", "Access-Control-Request-Method")
 		origin := r.Header.Get("Origin")
 		if origin != "" && len(app.config.cors.trustedOrigins) != 0 {
-
-		}
-		for i := range app.config.cors.trustedOrigins {
-			if origin == app.config.cors.trustedOrigins[i] {
-				w.Header().Set("Access-Control-Allow-Origin", origin)
+			for i := range app.config.cors.trustedOrigins {
+				if origin == app.config.cors.trustedOrigins[i] {
+					w.Header().Set("Access-Control-Allow-Origin", origin)
+					// Check for the existence of HTTP Method OPTIONS and contains the
+					// "Access-Control-Request-Method" header. If it does, treat it as
+					// a pre-flight request and add necessary preflight response headers.
+					if r.Method == http.MethodOptions &&
+						r.Header.Get("Access-Control-Request-Method") != "" {
+						w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, PUT, PATCH, DELETE")
+						w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+						w.WriteHeader(http.StatusOK)
+						return
+					}
+				}
 			}
 		}
 		next.ServeHTTP(w, r)
