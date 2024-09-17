4b16488a6853188ce6c1afc26749bf24e5b5a888
PrayasPathak <prayasroman1@gmail.com>
2024-09-17 16:34:57 +0545

    Simple CORS setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"flag"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -37,6 +38,9 @@ type config struct {
 		password string
 		sender   string
 	}
+	cors struct {
+		trustedOrigins []string
+	}
 }
 
 type application struct {
@@ -65,6 +69,11 @@ func main() {
 		"SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "c6f81c61cbe972", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.net>", "SMTP sender")
+
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 
 	flag.Parse()
 
